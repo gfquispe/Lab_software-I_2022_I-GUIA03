37077f4762e4d65b13c40df84109fcde3d2be3f4
Gerar Quispe <fryzito@gmail.com>
2022-06-29 16:18:05 -0500

    feat(database): Update SLQ script, adding two new attributes created and updated

--- Material-02/alumnos.sql
+++ Material-02/alumnos.sql
@@ -18,7 +18,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-
 --
 -- Estructura de tabla para la tabla `alumnos`
 --
@@ -27,20 +26,18 @@ CREATE TABLE `alumnos` (
   `cod_estudiante` int(11) NOT NULL,
   `dni` int(11) NOT NULL,
   `nombres` varchar(20) NOT NULL,
-  `apellidos` varchar(30) NOT NULL
+  `apellidos` varchar(30) NOT NULL,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `alumnos`
 --
 
-INSERT INTO `alumnos` (`cod_estudiante`, `dni`, `nombres`, `apellidos`) VALUES
-(101, 75854549, 'Dennis', 'Candia Sanches'),
-(103, 75854551, 'Jimena', 'Sandobal Gutierre'),
-(105, 75854553, 'Gerardo', 'Quispe Huaman'),
-(106, 23454553, 'Manuel', 'Herrera Lopez');
+INSERT INTO `alumnos` (`cod_estudiante`, `dni`, `nombres`, `apellidos`, `created_at`, `updated_at`) VALUES
+(101, 75854549, 'Dennis', 'Candia Sanches', '2022-06-29 20:41:46', '2022-06-29 20:41:46'),
+(103, 75854551, 'Jimena', 'Sandobal Gutierre', '2022-06-29 20:41:46', '2022-06-29 20:41:46'),
+(105, 75854553, 'Gerardo', 'Quispe Huaman', '2022-06-29 20:41:46', '2022-06-29 20:41:46'),
+(106, 23454553, 'Manuel', 'Herrera Lopez', '2022-06-29 20:41:46', '2022-06-29 20:41:46');
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
